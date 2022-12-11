e9a30256c92a7e967c12a340d8159970023ffc2d
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-12-12 00:59:01 +0530

    jh

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-login.component.css']
 })
 export class AdminLoginComponent {
+  constructor(private route:Router){
+    localStorage.removeItem("userInfo");
+  }
+  username=""
+  password=""
+  raedvalue=()=>{
+    if(this.username=="admin"&&this.password=="12345"){
+      this.route.navigate(["/viewEmp"])
+    }else{
+      alert("Invaild")
+    }
+  }
 
 }
